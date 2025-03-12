b0d62d5fb12635001bd6b9d8eb9895aa61b1e794
Roznrasho <roznrasho@gmail.com>
2025-03-12 10:49:38 +0100

    refactor App component to manage state for input text and QR code, and clean up unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [, ] = useState(0)
+  const [inputText, setIinputText] = useState("");
+  const [qrCodeUrl, setQRCodeUrl] = useState("");
+  const [showPhoneButtons, setShowPhoneButtons] = useState(false);
+  const [showTextButtons, setShowTextButtons] = useState(false);
+  // const [showEmailButtons, setShowEmailButtons] = useState(false);
+  // const [showSocialButtons, setShowSocialButtons] = useState(false);
+  // const [showLocationButtons, setShowLocationButtons] = useState(false);
+  // const [showWifiButtons, setShowWifiButtons] = useState(false);
+  // const [showUrlButtons, setShowUrlButtons] = useState(false);
+  // const [showSMSButtons, setShowSMSButtons] = useState(false);
 
-  return (
-    <>
-      
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
