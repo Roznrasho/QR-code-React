c7fb420214504bf93c7f23ef031b0944be591a1d
Roznrasho <roznrasho@gmail.com>
2025-03-12 21:45:29 +0100

    add QR code fetching functionality and integrate Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import "./App.css";
 
 function App() {
   const [inputText, setIinputText] = useState("");
   const [qrCodeUrl, setQRCodeUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<null | Error>(null);
   const [showPhoneButtons, setShowPhoneButtons] = useState(false);
   const [showTextButtons, setShowTextButtons] = useState(false);
   // const [showEmailButtons, setShowEmailButtons] = useState(false);
@@ -13,6 +15,23 @@ function App() {
   // const [showWifiButtons, setShowWifiButtons] = useState(false);
   // const [showUrlButtons, setShowUrlButtons] = useState(false);
   // const [showSMSButtons, setShowSMSButtons] = useState(false);
+
+  const fetchQRCode = async (data: string) => {
+    const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(
+      data
+    )}`;
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error("Failed to generate QR code");
+      }
+    } catch (error: unknown) {
+      setError(error as Error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  
 
   return <></>;
 }
